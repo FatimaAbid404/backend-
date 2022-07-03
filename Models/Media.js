bce35268694c4c6f1ee58709ff55b9934980ed8a
FatimaAbid <abidfatima404@gmail.com>
2022-07-03 09:15:18 -0700

    .

---
+++ Models/Media.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const MediaSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    keywords: {
+      type: String,
+      required: true,
+    },
+    videos: [{ type: String }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Media = mongoose.model("Media", MediaSchema);
